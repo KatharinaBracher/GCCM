78e79e87c5bc9964586c13a63f96df98649d51f2
Katharina Bracher <katharina.bracher@mailbox.org>
2024-06-25 15:47:35 +0100

    coregister data and rename notebooks

--- Gao_code/raster/HeavyMetal.R
+++ Gao_code/raster/HeavyMetal.R
@@ -28,8 +28,7 @@ source("basic.r")
 source("GCCM.r")
 
 
-#xImage<-readGDAL("dTRI.tif")     #read the cause variable 
-xImage<-readGDAL("nlights03.tif")  
+xImage<-readGDAL("dTRI.tif")     #read the cause variable 
 yImage<-readGDAL("Cu.tif")       #read the effect variable 
  
 plot(xImage)                   #plot the cause variable 
@@ -39,12 +38,11 @@ xMatrix<-as.matrix(xImage)
 yMatrix<-as.matrix(yImage)
 
 
-lib_sizes<-seq(10,120,20)   # library sizes, will be the horizontal ordinate  of the result plot. Note here the lib_size is the window size
+#lib_sizes<-seq(10,120,20)   # library sizes, will be the horizontal ordinate  of the result plot. Note here the lib_size is the window size
                             # The largest value ('to' parameter) can be set to the largest size of image (the minor of width and length)
                             # the 'by' can be set by taking account to the computation time
 # use only 3 Ls (takes 1h)
-#lib_sizes<-seq(10,120,50)
-#lib_sizes<-seq(110,110,110)
+lib_sizes<-seq(10,120,50)
 
 E<-3                           # the dimensions of the embedding   
 lib<-NULL
@@ -88,12 +86,12 @@ colnames(y_xmap_x_interval)<-c("y_xmap_x_upper","y_xmap_x_lower")  #calculate th
 results<-data.frame(lib_sizes,x_xmap_y_means,y_xmap_x_means,x_xmap_y_Sig,y_xmap_x_Sig,x_xmap_y_interval,y_xmap_x_interval)  #Save the cross-mapping prediction results
 
 
-write.csv(results,file="Nresults.csv")     
+write.csv(results,file="dTRI_nlights03.csv")     
 
 par(mfrow=c(1,1))
 par(mar=c(5, 4, 4, 2) + 0.1)
 
-jpeg(filename = "corrected_results.jpg",width = 600, height = 400)     #Plot the cross-mapping prediction results
+jpeg(filename = "dTRI_nlights03.jpg",width = 600, height = 400)     #Plot the cross-mapping prediction results
 plot(lib_sizes, x_xmap_y_means, type = "l", col = "royalblue", lwd = 2, 
      xlim = c(min(lib_sizes), max(lib_sizes)), ylim = c(0.0, 1), xlab = "L", ylab = expression(rho))
 lines(lib_sizes, y_xmap_x_means, col = "red3", lwd = 2)
