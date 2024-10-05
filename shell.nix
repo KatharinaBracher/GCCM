b85150a80ae86cf19fddf590d4380804bfc6e304
Max Ammann <max@maxammann.org>
2024-10-05 15:58:34 +0200

    Add poetry project and nix shell

--- shell.nix
+++ shell.nix
@@ -7,13 +7,15 @@ let
     })
     { };
 in
-pkgs.mkShell {
-  nativeBuildInputs = [
-    pkgs21.R
-    pkgs21.rPackages.rgdal
-    pkgs21.rPackages.foreach
-    pkgs21.rPackages.doParallel
+pkgs.mkShell rec {
+  buildInputs = [
+    pkgs.python3
+    pkgs.poetry
+    pkgs.zlib
   ];
+
   shellHook = ''
+    export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH"
+    export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:$LD_LIBRARY_PATH"
   '';
 }
