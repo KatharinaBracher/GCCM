5f9d39079d1b65500a21708c425c8e942457ada0
Katharina Bracher <katharina.bracher@mailbox.org>
2024-10-05 15:40:29 +0200

    add id

--- code/grid_diffusionMean.py
+++ code/grid_diffusionMean.py
@@ -4,11 +4,12 @@ from diffusion import remove_linear_signals, run_sim
 from optimalEmbedding_sampling import run_optEmbedding_sampling
 from GCCM_sampling import run_GCCM_sampling
 import pickle
+import uuid
 from multiprocessing import Pool
 
-def run_sample(sample, size, c, a1, a2):
+def run_sample(sample, size, c, a1, a2, uuid):
     results = {}
-    filename = 'results_test/c'+str(c)+'_a1'+str(a1)+'_a2'+str(a2)+'.pkl'
+    #filename = 'results_test/c'+str(c)+'_a1'+str(a1)+'_a2'+str(a2)+'.pkl'
 
     print('running with c=', c, 'and a=', a1, a2)
     
@@ -18,16 +19,15 @@ def run_sample(sample, size, c, a1, a2):
         #Y_rand = np.random.rand(size, size)
         #X, Y = run_sim(X_rand, Y_rand, T=T, c=c, a1=a1, a2=a2, plot=False)
         #correlation_coefficient, p_value = pearsonr(X.flatten(), Y.flatten())
-        #if correlation_coefficient < 0.35:
-        print('running with seed', s)
         #conv = run_GCCM_sampling(X, Y, lib_sizes, E=5, cores=6)
         conv = None
         correlation_coefficient, p_value = None, None
         results[s] = {'corr':[correlation_coefficient, p_value], 
                       'gccm':conv}
+
+    return results
         
-    with open(filename, 'wb') as pickle_file:
-        pickle.dump(results, pickle_file)
+    
             
 def run_grid():
 
@@ -45,4 +45,15 @@ def run_grid():
     a_list = np.dstack((a1,a2)).squeeze() 
     
     with Pool() as p:
-        p.starmap(run_sample, [(sample, size, c ,a1 ,a2) for c in c_list for (a1, a2) in a_list])
+        idx = 1
+        parameter_list = [(sample, size, c ,a1 ,a2, uuid.uuid4()) for c in c_list for (a1, a2) in a_list]
+        results_list = p.starmap(run_sample, parameter_list)
+
+        meta = {parameter[5]: parameter for parameter in parameter_list}
+        results_map = {parameter[5]: result for parameter, result in zip(parameter_list, results_list)}
+    
+        with open('results_test/test', 'wb') as pickle_file:
+            pickle.dump({'meta':meta, 'results_map':results_map}, pickle_file)
+
+if __name__ == "__main__":
+    run_grid()
